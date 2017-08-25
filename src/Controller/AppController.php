6473b2f74b0ac80c158a56bfaccc7d56f93fbda1
Xili Chen <xche302@student.monash.edu>
2017-08-25 10:23:34 +1000

    Revert the Login functionality
    Delete related files and methods, as it causes bugs for now.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -27,31 +27,7 @@ use Cake\Event\Event;
  */
 class AppController extends Controller
 {
-    public function initialize()
-{
-    $this->loadComponent('Flash');
-    $this->loadComponent('Auth', [
-        'authenticate' => [
-            'Form' => [
-                'fields' => [
-                    'username' => 'email',
-                    'password' => 'password'
-                ]
-            ]
-        ],
-        'loginAction' => [
-            'controller' => 'Users',
-            'action' => 'login'
-        ],
-        'unauthorizedRedirect' => $this->referer() // If unauthorized, return them to page they were just on
-    ]);
-
-    // Allow the display action so our pages controller
-    // continues to work.
-    $this->Auth->allow(['display']);
-}
-
-    /**
+       /**
      * Initialization hook method.
      *
      * Use this method to add common initialization code like loading components.
