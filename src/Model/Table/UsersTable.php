a086ec709963ec32dce7f41c49fd4703d74a1f15
nmlob1 <31327795+nmlob1@users.noreply.github.com>
2017-09-19 21:45:09 +1000

    Validation v.1
    Validation of field to numbers/characters . Confirm password, calculation of age to submit guardian details still required, photo consent required still to be added

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -49,26 +49,115 @@ class UsersTable extends Table
     {
         $validator
             ->integer('id')
-            ->allowEmpty('id', 'create');
+            ->allowEmpty('id', 'create'); //id
 
         $validator
             ->email('email')
             ->requirePresence('email', 'create')
-            ->notEmpty('email');
+            ->notEmpty('email','Please fill this field'); //email
 
+        $validator
+            ->requirePresence('first_name', 'create')
+            ->notEmpty('first_name','This field is required')
+            ->add('first_name', 'custom', array(
+                        'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                        'message' => 'Characters only'
+            ));
 
-       // $validator
-         //         ->add('f_name', 'custom', array(
-           //             'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
-             //           'message' => 'Please a valid name'
-              //          ))
-        //->requirePresence('f_name', 'create')
-        //->notEmpty('f_name');
+        $validator
+            ->requirePresence('last_name', 'create')
+            ->notEmpty('last_name','This field is required')
+            ->add('last_name', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
 
+        $validator
+            ->requirePresence('school', 'create')
+            ->notEmpty('school')
+            ->add('school', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->requirePresence('mobile', 'create')
+            ->notEmpty('mobile')
+            ->add('mobile','custom',array(
+                'rule'=> array ('custom',['minLength',10]),
+                'message'=>'Require at least 10 digits'
+            ))
+            ->add('mobile','custom',array(
+                'rule'=> array ('custom',['maxLength',14]),
+                'message'=>'Maximum 14 digits'
+            ))
+            ->add('mobile','custom',array(
+                'rule'=> array ('custom',['numeric']),
+                'message'=>'Numbers only'
+            ));
+
+        $validator
+            ->requirePresence('street', 'create')
+            ->notEmpty('street','This field is required')
+            ->add('street', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->requirePresence('suburb', 'create')
+            ->notEmpty('suburb','This field is required')
+            ->add('suburb', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->requirePresence('postcode', 'create')
+            ->notEmpty('postcode','This field is required')
+            ->add('postcode','custom',array(
+                'rule'=> array ('custom',['numeric']),
+                'message'=>'Numbers only'
+            ));
+
+        $validator
+            ->requirePresence('state', 'create')
+            ->notEmpty('state','This field is required')
+            ->add('state', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->requirePresence('country', 'create')
+            ->notEmpty('country','This field is required')
+            ->add('country', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->requirePresence('g_fname', 'create')
+            ->add('g_fname', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->requirePresence('g_lname', 'create')
+            ->add('g_lname', 'custom', array(
+                'rule' => array('custom', '/^[a-zA-Z\s]*$/') ,
+                'message' => 'Characters only'
+            ));
+
+        $validator
+            ->email('g_email')
+            ->requirePresence('g_email', 'create');
 
         $validator
             ->requirePresence('password', 'create')
-            ->notEmpty('password');
+            ->notEmpty('password','Password required');
+        
         $validator
             ->date('dob')
             ->notEmpty('dob', 'Please fill this field');
